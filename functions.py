4d6a5a1c8fd9b6d965fb016c9a979aa3c96959b8
boloson <londonbolo@gmail.com>
2019-06-18 13:53:16 +1200

    Updated algorithm of parsing CDF file

--- functions.py
+++ functions.py
@@ -1,51 +1,43 @@
-import numpy as np
-import pandas as pd
-from netCDF4 import Dataset
+SCAN_INDEX = 'scan_index'
+POINT_COUNT = 'point_count'
+MASS_VALUES = 'mass_values'
+INTENSITY_VALUES = 'intensity_values'
+SCAN_ACQ_TIME = 'scan_acquisition_time'
 
-def build_df(file_name, time_as_column=False):
-  '''
-  Build intensity matraix dataframe
-  '''
-  dataset = Dataset(file_name, 'r')
+scan_indexes = dataset.variables[SCAN_INDEX]
+mz_values = dataset.variables[MASS_VALUES]
+point_counts =  dataset.variables[POINT_COUNT]
+intensity_values = dataset.variables[INTENSITY_VALUES]
+time_val = np.array([t.data for t in dataset.variables[SCAN_ACQ_TIME]])
+
+mz_max = int(round(np.max(mz_values), 0))
+mz_min = int(round(np.min(mz_values), 0))
+num_mz = mz_max - mz_min + 1
+point_upper_bound = len(intensity_values) - 1
+
+scan_list = []
+time_list = []
+
+start = time.time()
+
+for i in range(len(scan_indexes)):
   
-  __MASS_STRING = "mass_values"
-  __INTENSITY_STRING = "intensity_values"
-  __TIME_STRING = "scan_acquisition_time"
+  start_i = scan_indexes[i]
+  num_point = point_counts[i]
+
+  if num_point == 0 :
+    continue
+  time_row =  np.zeros(num_mz)
   
-  intensity_values = np.array(dataset.variables[__INTENSITY_STRING])
-  intensity_list = []
-  intensity_previous = intensity_values[0]
-  intensity_list.append(intensity_previous)
-
-  mz_strings = np.array(dataset.variables[__MASS_STRING])
-  intensity = np.array(dataset.variables[__INTENSITY_STRING])
-  time_list = np.array(dataset.variables[__TIME_STRING])
-
-  mz_values = mz_strings.round().astype(np.int16)
-  intensity_list = []
-  scan_list = []
-
-  mz_max = mz_values.max()
-  mz_min = mz_values.min()
-
-  num_mz = len(range(mz_min, mz_max)) + 1
-
-  time_row = np.zeros(num_mz)
-
-  mz_prev = mz_values[0]
-
-  for i in range(len(mz_values)):
-    mz = mz_values[i]
-    if mz_prev <= mz:
-      time_row[mz - mz_min] = intensity[i]
-      mz_prev = mz
-
-    else:
-      scan_list.append(time_row)
-      time_row = np.zeros(num_mz)
-      time_row[mz - mz_min] = intensity[i]
-      mz_prev = mz
-  scan_list.append(time_row)   # add last row
+  value_index = start_i
+  for incr in range(num_point):
+    cur_i = start_i + incr
+    mz = int(round(mz_values[cur_i]))
+    intensity_val = intensity_values[cur_i]
+    time_row[mz-mz_min] = time_row[mz-mz_min] + intensity_val
+    
+  scan_list.append(time_row)
+  time_list.append(time_val[i])
   
   if time_as_column:
     df = pd.DataFrame(np.transpose(scan_list), columns=time_list, index=range(mz_min, mz_max+1))
